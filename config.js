6bae2159277ba4feaddd2102895d4ee2755adf8e
lidushihuaidan <125271040+lidushihuaidan@users.noreply.github.com>
2024-03-14 13:34:30 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
-// 霸都丶傲天 2019年10月10日 https://github.com/AJLoveChina/birthday
+// 阿卷 2024年3月15日 https://github.com/AJLoveChina/birthday
 var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
         "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
+        "心爱的雯雯",  // 同上...
         "今天是你的生日",
         "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "第一个生日",
+        "往后的每一个生日",
+        "都有我陪你",
+        "祝你今年要更加开心和健康",
+        "祝你可以想睡就睡想吃就吃",
+        "祝你少点emo多点爱我",
+        "加油！",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -32,10 +32,10 @@ var config = {
     desc: {
         turn_on: "开始",
         play: "音乐",
-        bannar_coming: "颜色",
+        bannar_coming: "祝福",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        cake_fadein: "蛋糕",
+        light_candle: "蜡烛",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
